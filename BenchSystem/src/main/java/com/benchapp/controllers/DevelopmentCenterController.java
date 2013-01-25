ac4e9f3399bc372bf8385ac69ed2cdacb564baa3
Esteban <eestloro92@gmail.com>
2013-01-25 11:19:54 -0600

    json change

--- BenchSystem/src/main/java/com/benchapp/controllers/DevelopmentCenterController.java
+++ BenchSystem/src/main/java/com/benchapp/controllers/DevelopmentCenterController.java
@@ -3,30 +3,42 @@
  */
 package com.benchapp.controllers;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.annotation.Resource;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.benchapp.dao.DevelopmentCenterDAO;
+import com.benchapp.models.DevelopmentCenter;
+
 /**
  * @author eduardo.bran
- *
+ * 
  */
 
 @Controller
-@RequestMapping("/welcome")
+@RequestMapping("/DevelopmentCenter/")
 public class DevelopmentCenterController {
 
 	@Resource(name = "developmentCenterImplDAO")
 	private DevelopmentCenterDAO developmentCenterDAO;
 
-	@RequestMapping(method = RequestMethod.GET)
-	public String ListDC(ModelMap model)  {
-	model.addAttribute("developmentCenter",developmentCenterDAO.AvanticaDevelopmentCenter());
-	return "Lista";
+	@RequestMapping(value = "List", method = RequestMethod.GET, produces =  "application/json;charset=utf8")
+	@ResponseStatus(value = HttpStatus.OK)
+	@ExceptionHandler(NullPointerException.class)
+	@ResponseBody
+	public Map<String,List<DevelopmentCenter>> ListDC(NullPointerException nullPointer) {
+		Map<String,List<DevelopmentCenter>> jsonDevelopmentCenter =  new HashMap<String,List<DevelopmentCenter>>();
+		jsonDevelopmentCenter.put("aaData",developmentCenterDAO.AvanticaDevelopmentCenter());
+		return jsonDevelopmentCenter;
 	}
 }
